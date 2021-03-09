9ac745a9b5fb7ed35f491f77728a6c7cf14a652e
Christina Cook <cmc.cook7@gmail.com>
2021-03-09 10:28:06 -0700

    Set up fixtures and it blocks in tests

---
+++ cypress/integration/url_shortener.spec.js
@@ -0,0 +1,21 @@
+describe('Url Shortener', () => {
+  it('Should view the page title when visiting the page', () => {
+
+  })
+
+  it('Should view the existing shortened urls when visiting the page', () => {
+
+  })
+
+  it('Should view a form when visiting the page', () => {
+
+  })
+
+  it('Should see information reflected in the form after entering text', () => {
+
+  })
+
+  it('Should see a new shortened url on the page after submitting the form', () => {
+    
+  })
+})
